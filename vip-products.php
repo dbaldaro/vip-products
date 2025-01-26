fce23805fd93e580ca96a9a792a300baadd0b76f
dbaldaro <david.baldaro@xmpie.com>
2025-01-26 19:17:03 +0000

    version 1.1.20

--- vip-products.php
+++ vip-products.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: WooCommerce VIP Products
 Description: Allows for creation of VIP-exclusive products for specific users.
-Version: 1.1.14
+Version: 1.1.20
 Author: David Baldaro
 */
 
